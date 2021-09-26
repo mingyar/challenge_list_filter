cfd7d299d0779b4ace8e02737d8e4cdf97ec9514
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-09-26 19:37:58 -0300

    Initial commit

---
+++ lib/challenge_list_filter.ex
@@ -0,0 +1,18 @@
+defmodule ChallengeListFilter do
+  @moduledoc """
+  Documentation for `ChallengeListFilter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChallengeListFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/challenge_list_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule ChallengeListFilterTest do
+  use ExUnit.Case
+  doctest ChallengeListFilter
+
+  test "greets the world" do
+    assert ChallengeListFilter.hello() == :world
+  end
+end
